cbda59f6933ca0a383be39f04684bb997494821e
Mbianou Bradon <mbianoubradon2000@gmail.com>
2022-12-24 08:17:51 +0100

    Preparing for new challenge

--- public/Dec 23, 2022/main.js
+++ public/Dec 23, 2022/main.js
@@ -1,5 +1,35 @@
+const testimonial1 = document.getElementById("testimonial1");
+const testimonial2 = document.getElementsById("testimonial2");
+const testimonial3 = document.getElementsById("testimonial3");
+const previous = document.getElementById("prev");
+const next = document.getElementById("next");
+
+let testimonial = [testimonial1,testimonial2, testimonial3]
+let vue = []
+vue = document.getElementsByClassName("vue");
+
 let testimonialIndex = 1;
 showTestimonial(testimonialIndex);
+
+function showTestimonial(n) {
+  let i;
+
+  if (n > testimonial.length) {testimonialIndex = 1}
+
+  if (n < 1) {testimonialIndex = testimonial.length}
+
+  if (i = 0 || i < testimonial.length) {
+
+    testimonial[i].style.display = "none";
+  }
+
+  for (i = 0; i < vue.length; i++) {
+    vue[i].className = vue[i].className.replace(" active", "");
+  }
+  
+  testimonial[testimonialIndex-1].style.display = "flex";
+  vue[testimonialIndex-1].className += " active";
+}
 
 // Next/previous controls
 function plusTestimonial(n) {
@@ -11,22 +41,10 @@ function currentTestimonial(n) {
   showTestimonial(testimonialIndex = n);
 }
 
-function showTestimonial(n) {
-  let i;
-  let testimonial = document.getElementsByClassName("testimonial");
-  let vue = document.getElementsByClassName("vue");
+previous.addEventListener("click", ()=>{
+   plusTestimonial(-1)
+})
 
-  if (n > testimonial.length) {testimonialIndex = 1}
-
-  if (n < 1) {testimonialIndex = testimonial.length}
-  // for (i = 0; i < testimonial.length; i++) {
-  //   testimonial[i].style.display = "none";
-  // }
-
-  for (i = 0; i < dots.length; i++) {
-    vue[i].className = vue[i].className.replace(" active", "");
-  }
-  
-  testimonial[testimonialIndex-1].style.display = "block";
-  vue[testimonialIndex-1].className += " active";
-}
+next.addEventListener("click", ()=>{
+   plusTestimonial(1)
+})

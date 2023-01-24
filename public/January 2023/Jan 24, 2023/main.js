c9f0f51a1e9cbc688f38c33ae0aac2b6d6b6dbd4
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-01-24 13:38:19 +0100

    Challenge completed

--- public/January 2023/Jan 24, 2023/main.js
+++ public/January 2023/Jan 24, 2023/main.js
@@ -1,5 +1,9 @@
 const watch = document.getElementById('watch')
 const tooltip = document.getElementById('tooltip')
+const modal = document.getElementById("myModal");
+const close = document.getElementById("close");
+const main = document.getElementById("main")
+const subMain = document.getElementById("sub_main")
 
 watch.addEventListener ('mouseenter', ()=>{
     tooltip.classList.remove('hidden')
@@ -8,3 +12,30 @@ watch.addEventListener ('mouseenter', ()=>{
 watch.addEventListener('mouseleave',()=>{
     tooltip.classList.add('hidden')
 })
+
+
+// When the user clicks the button, open the modal 
+watch.onclick = function() {
+  modal.style.display = "block";
+  tooltip.classList.add('hidden')
+  main.classList.add('backdrop-blur-sm')
+  subMain.classList.add('backdrop-blur-sm')
+}
+
+// When the user clicks on <span> (x), close the modal
+close.addEventListener("click", ()=>{
+    modal.style.display = "none";
+    tooltip.classList.add('hidden')
+    main.classList.remove('backdrop-blur-sm')
+    subMain.classList.remove('backdrop-blur-sm')
+})
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    main.classList.remove('backdrop-blur-sm')
+    subMain.classList.remove('backdrop-blur-sm')
+  }
+}

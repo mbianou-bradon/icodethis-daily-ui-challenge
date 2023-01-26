92a310a0a1d0ad8a2f9d4daf2f0e9ae721148e39
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-01-26 08:09:43 +0100

    Completeed challenge

--- public/January 2023/Jan 25, 2023/index.js
+++ public/January 2023/Jan 25, 2023/index.js
@@ -1,6 +1,8 @@
+
 const root = document.getElementById('root')
 const newAlert = document.getElementById('newAlert')
 const close = document.querySelectorAll(".close")
+const tooltip = document.getElementById("cool");
 
 console.log(close)
 
@@ -20,6 +22,7 @@ const moreAlert = [
 
 newAlert.addEventListener("click", () => {
     generateAlert();
+    tooltip.classList.add("hidden")
 });
 
 function generateAlert(givenAlert) {
@@ -55,4 +58,14 @@ close.forEach((remove) => {
   remove.addEventListener('click', () => {
     remove.parentNode.remove();
   });
+})
+
+
+newAlert.addEventListener("mouseenter", ()=>{
+    tooltip.classList.remove("hidden")
+})
+
+
+newAlert.addEventListener("mouseleave", ()=>{
+    tooltip.classList.add("hidden")
 })

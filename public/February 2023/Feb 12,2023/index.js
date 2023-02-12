1edc96ebd8c57179485dbac0b68f46dfa8410077
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-02-12 09:58:55 +0100

    Added some small changes

--- public/February 2023/Feb 12,2023/index.js
+++ public/February 2023/Feb 12,2023/index.js
@@ -1,3 +1,16 @@
-document.getElementById("btn").addEventListener("click", ()=> {
-    document.getElementById("more").classList.toggle("hidden");
-})
+
+const btn = document.getElementById("btn");
+const btn_inner = document.getElementById("inner")
+
+    btn.addEventListener("click", () => {
+        document.getElementById("more").classList.toggle("hidden");
+        const text = "Show more"
+
+        if (inner.innerText = text ) {
+         inner.innerText = "Show less"
+        }
+
+        if (inner.innerText != text) {
+         inner.innerText = "Show more"
+        }
+    })

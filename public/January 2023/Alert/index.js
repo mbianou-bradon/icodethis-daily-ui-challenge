220a3d72003876f8f8c3cf8037fe7af088f2c766
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-03-29 11:16:35 +0100

    Alert Article

--- public/January 2023/Alert/index.js
+++ public/January 2023/Alert/index.js
@@ -14,7 +14,7 @@ const moreAlert = [
     },
     {
         color : `red-500`,
-        textMessage : "Your object couldn't been Created !",
+        textMessage : "Your object couldn't be Created !",
         icon : `
         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="m10.25 5.75l-4.5 4.5m0-4.5l4.5 4.5"/><circle cx="8" cy="8" r="6.25"/></g></svg>`
     }
